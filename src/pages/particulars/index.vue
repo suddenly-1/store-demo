3a5b404b99da2a5ce0cc66261aa61ae20e085470
suddenly <2676178516@qq.com>
2019-09-06 10:53:57 +0800

    router ready

---
+++ src/pages/particulars/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    particulars
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'particulars'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,45 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import home from 'pages/home'
+import category from 'pages/category'
+import cart from 'pages/cart'
+import personal from 'pages/personal'
+import particulars from 'pages/particulars'
+import search from 'pages/search'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: []
+  routes: [
+    {
+      name: 'home',
+      path: '/home',
+      component: home
+    },
+    {
+      name: 'category',
+      path: '/category',
+      component: category
+    },
+    {
+      name: 'cart',
+      path: '/cart',
+      component: cart
+    },
+    {
+      name: 'personal',
+      path: '/personal',
+      component: personal
+    },
+    {
+      name: 'particulars',
+      path: '/particulars',
+      component: particulars
+    },
+    {
+      name: 'search',
+      path: '/search',
+      component: search
+    }
+  ]
 })
